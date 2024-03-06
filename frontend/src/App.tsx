d19973b3ca7ebccda9efb55d31813db854d6b115
madhavsahi <madhavsahi16@gmail.com>
2024-03-06 15:38:22 +0530

    bit of backend modifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
 // import React from 'react'
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Blog from "./pages/Blogs";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+
 const App = () => {
   return (
     <>
-      <div>
-        <p>Hey ReactJS</p>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
