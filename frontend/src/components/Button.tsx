c25fff3f7ea2ae9bc5cf4ff8a7ae80a60e6d9ea8
Madhav Sahi <63411260+MadhavSahi@users.noreply.github.com>
2024-07-14 06:13:02 +0530

    Update Button.tsx

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -5,7 +5,7 @@ const Button = () => {
   return (
     <Link to="/publish">
       <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full my-6">
-        Add New Blog!
+        Add New Blog !
       </button>
     </Link>
   );
